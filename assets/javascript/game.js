b579ea36c87ac5955b8d223ff8678f3e0b9a931d
jdalton467 <jamesdalton463@gmail.com>
2017-05-29 14:18:38 -0400

    push

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+	
+var score = 0;
+var win = 0;
+var lose = 0;
+
+var crystal = {diamond : [Math.floor(Math.random()*(12-1))+1],
+							 emerald : [Math.floor(Math.random()*(12-1))+1],
+               ruby: [Math.floor(Math.random()*(12-1))+1],
+               sapphire: [Math.floor(Math.random()*(12-1))+1],    
+             };
+
+console.log(crystal.diamond);
+console.log(crystal.emerald);
+console.log(crystal.ruby);
+console.log(crystal.sapphire);
+
+
+
+
+
+$("#randomnumber").html([Math.floor(Math.random()*(120-19))+19]); //randomnumber generated that the player tries to match
+$("#winbox").html(win);
+$("#losebox").html(lose);
+$("#score").html(score);
+
+
+
+
